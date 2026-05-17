5367c68cdcdf023f06d24b0b9735e3d7b7236dba
Andrew Wittrock <andy@wittrock.us>
2026-05-17 14:17:35 -0500

    Add FieldRegistry to only allow some fields to be updated via the API

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
+import { FieldRegistry } from "@core/express/util";
 import { init } from "../theming/migrations/00-init";
 import { addGlobalStyle } from "../theming/migrations/01-addGlobalStyle";
 
 export { apiConfig } from "./endpoints";
-console.log("theming test");
+
 export const themingMigrations = [init, addGlobalStyle];
 export const setupMigrations = [init];
+
+FieldRegistry.register(
+    "themes",
+    ["name", "description", "imageUrl", "globalStyles", "json", "enabled"]
+);
