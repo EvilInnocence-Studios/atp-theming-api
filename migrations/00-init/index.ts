228f8ff2fbea94fa6de82185b8ded5a80ea01c6c
Andrew Wittrock <andy@wittrock.us>
2026-08-04 20:58:19 -0500

    Update migration priority

--- migrations/00-init/index.ts
+++ migrations/00-init/index.ts
@@ -7,32 +7,32 @@ import { themesTable } from "../tables";
 const db = database();
 
 const permissions = [
-    { name: "theme.view",       description: "Can view themes"   },
-    { name: "theme.update",     description: "Can update themes" },
-    { name: "theme.create",     description: "Can create themes" },
-    { name: "theme.delete",     description: "Can delete themes" },
+    { name: "theme.view", description: "Can view themes" },
+    { name: "theme.update", description: "Can update themes" },
+    { name: "theme.create", description: "Can create themes" },
+    { name: "theme.delete", description: "Can delete themes" },
 
-    { name: "sass.compile",     description: "Can compile sass"  },
+    { name: "sass.compile", description: "Can compile sass" },
 ];
 
 const rolePermissions = [
-    { roleName: "SuperUser", permissionName: "theme.view"   },
+    { roleName: "SuperUser", permissionName: "theme.view" },
     { roleName: "SuperUser", permissionName: "theme.update" },
     { roleName: "SuperUser", permissionName: "theme.create" },
     { roleName: "SuperUser", permissionName: "theme.delete" },
     { roleName: "SuperUser", permissionName: "sass.compile" },
-    { roleName: "Public",    permissionName: "theme.view"   },
+    { roleName: "Public", permissionName: "theme.view" },
 ];
 
 const settings = [
-    {key: "themeThumbnailFolder", value: "media/theme"},
+    { key: "themeThumbnailFolder", value: "media/theme" },
 ]
 
-export const init:IMigration = {
+export const init: IMigration = {
     name: "init",
     module: "theming",
     description: "Install the theming module",
-    order: 1,
+    order: 3,
     version: "1.0.0",
     down: () => {
         return db.schema
@@ -40,7 +40,7 @@ export const init:IMigration = {
     },
     up: () => {
         return db.schema
-        .createTable("themes", themesTable);
+            .createTable("themes", themesTable);
     },
     initData: async () => {
         await insertPermissions(db, permissions);
